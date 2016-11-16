c9ab67939fab3b2746bb38ffd4074a5429de48b0
Rabbit <venkatasivaprasadreddypulagam@gmail.com>
2016-11-16 18:14:18 +0530

    first commit along with required packages

---
+++ ProjectFinal/ProjectFinal/ProjectFinalPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ProjectFinal
+{
+	public partial class ProjectFinalPage : ContentPage
+	{
+		public ProjectFinalPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
